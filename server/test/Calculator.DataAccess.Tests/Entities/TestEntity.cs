a0b579cbea67fc6e1e80d1af1ff0a59ed847c1e3
Julia Tolipova <julia.tolipova@gmail.com>
2017-07-19 00:46:44 +0200

    added dataaccess test project

---
+++ server/test/Calculator.DataAccess.Tests/Entities/TestEntity.cs
@@ -0,0 +1,14 @@
+﻿using Calculator.DataAccess.Entities;
+
+namespace Calculator.DataAccess.Tests.Entities
+{
+    public class TestEntity : IEntity
+    {
+        public string Id { get; private set; }
+
+        public TestEntity(string id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ server/test/Calculator.DataAccess.Tests/Repositories/RepositoryBaseTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Calculator.DataAccess.Entities;
+using Calculator.DataAccess.Entities.Calculation;
+using Calculator.DataAccess.Repositories;
+using Calculator.DataAccess.Tests.Entities;
+using Xunit;
+
+namespace Calculator.DataAccess.Tests.Repositories
+{
+    public class RepositoryBaseTests
+    {
+        [Fact]
+        public void RepositoryBase_Should_Save_All_Items()
+        {
+            // Arrange
+            var sut = new RepositoryBase<TestEntity>();
+            
+            // Act
+            sut.Insert(new TestEntity("1"));
+            sut.Insert(new TestEntity("2"));
+            
+            // Assert
+            Assert.Equal(2, sut.GetAll().Count());
+        }
+    }
+}
